b1f232dec71024a03f2c827bbab1638c7d790fe4
Veronika <veroni3ka2000@gmail.com>
2019-06-17 16:59:15 +0700

    Class of graph

---
+++ CodeFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystAnalys_lr1
+{
+    public class Vertex
+    {
+        public int x, y;
+        public Vertex()
+        { }
+        public Vertex(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public class Edge
+    {
+        public int v1, v2;
+        public int Weight;
+        public Edge()
+        { }
+        public Edge(int v1, int v2, int Weight)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.Weight = Weight;
+        }
+    }
