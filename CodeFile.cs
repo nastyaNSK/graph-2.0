9df47f664f2baf5556dcaf593056071920aa677a
Veronika <veroni3ka2000@gmail.com>
2019-06-17 17:00:57 +0700

    build adjacency matrix

--- CodeFile.cs
+++ CodeFile.cs
@@ -35,3 +35,34 @@ namespace SystAnalys_lr1
             this.Weight = Weight;
         }
     }
+
+
+        //заполняет матрицу смежности
+        public void fillAdjacencyMatrix(int numberV, List<Edge> E, int[,] matrix)
+        {
+            for (int i = 0; i < numberV; i++)
+                for (int j = 0; j < numberV; j++)
+                    matrix[i, j] = 0;
+            for (int i = 0; i < E.Count; i++)
+            {
+                matrix[E[i].v1, E[i].v2] = E[i].Weight;
+                matrix[E[i].v2, E[i].v1] = E[i].Weight;
+            }
+        }
+
+        //заполняет матрицу инцидентности
+        public void fillIncidenceMatrix(int numberV, List<Edge> E, int[,] matrix)
+        {
+            for (int i = 0; i < numberV; i++)
+                for (int j = 0; j < E.Count; j++)
+                    matrix[i, j] = 0;
+            for (int i = 0; i < E.Count; i++)
+            {
+                matrix[E[i].v1, i] = 1;
+                matrix[E[i].v2, i] = 1;
+            }
+        }
+
+        
+    }
+}
